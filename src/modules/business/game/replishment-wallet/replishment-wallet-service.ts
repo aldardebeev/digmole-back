7b6f4b8cb676a3f4f09b145f72cbe8596dc7b517
debeev_aldar@geos.local <debeev_aldar@smartworld.team>
2024-04-19 17:44:28 +0700

    feat: blockchain scan

--- src/modules/business/game/replishment-wallet/replishment-wallet-service.ts
+++ src/modules/business/game/replishment-wallet/replishment-wallet-service.ts
@@ -15,22 +15,13 @@ export class ReplishmentWalletService {
     constructor(
         private readonly prismaService: PrismaService,
         @InjectQueue(EQueue.CHECK_INPUT_TRANSACTION) private readonly checkInputTransactionQueue: Queue,
-        // private currentBlockchainHeight = null,
-        // private BLOCKCHAIN_INDENT_AMOUNT = 10
+
     ) { }
 
     async initReplishmentCheckJob(): Promise<void> {
         await this.checkReplishmentTransaction();
 
-        await this.startReplishmentCheckJob();
     }
-
-    private async startReplishmentCheckJob(): Promise<void> {
-        const jobId = randomUUID();
-        const jobOptions = { repeat: { every: 10000 } };
-        (await this.checkInputTransactionQueue.add("jobId", {}, jobOptions)).log('Job added');
-    }
-
     async checkReplishmentTransaction(): Promise<void> {
         const address = 'rod1js4u86dwmajqmwhjyk4zsju0s05sd7ypklzf2vqepvrus3gxnnksdp5j8s';
         const url = `https://mainnet.umi.top/api/addresses/${address}/transactions?limit=10&offset=-1`;
@@ -44,36 +35,65 @@ export class ReplishmentWalletService {
         } catch (error) {
         }
     }
-  
-    //  async  getBlockchainHeight() {
+
+    // private currentBlockchainHeight = null;
+    // private BLOCKCHAIN_INDENT_AMOUNT = 10;
+    // private maxAmountProcesses = 10
+
+
+    // async startCheckBlockNumbers(fromBlockNumber, toBlockNumber: number) {
+    //     let currentAmountProcesses = 0;
+
+    //     while (fromBlockNumber < toBlockNumber && currentAmountProcesses < this.maxAmountProcesses) {
+
+    //         this.startReplishmentCheckJob(fromBlockNumber);
+
+    //         fromBlockNumber++;
+    //         currentAmountProcesses++;
+    //     }
+    //     this.updateLastCheckedBlock(fromBlockNumber);
+    // }
+
+    // async getBlockchainHeight() {
     //     if (this.currentBlockchainHeight === null) {
     //         try {
     //             const latestBlockNumber = await this.getLatestBlockNumber();
     //             this.currentBlockchainHeight = latestBlockNumber - this.BLOCKCHAIN_INDENT_AMOUNT;
     //         } catch (error) {
     //             console.error('Error fetching latest block number:', error);
-    //             throw error; 
+    //             throw error;
     //         }
     //     }
 
     //     return this.currentBlockchainHeight;
     // }
-    
-    
-    // async  getLatestBlockNumber() {
-    // try {
-    //     const response = await axios.get('https://mainnet.umi.top/api/blocks?limit=1&offset=-1');
-    //     const data = response.data;
 
-    //     if (data && data.items && data.items.length > 0) {
-    //         return data.items[0].height;
-    //     } else {
-    //         throw new Error('No block data found');
+
+    // async getLatestBlockNumber() {
+    //     try {
+    //         const response = await axios.get('https://mainnet.umi.top/api/blocks?limit=1&offset=-1');
+    //         const data = response.data;
+
+    //         if (data && data.items && data.items.length > 0) {
+    //             return data.items[0].height;
+    //         } else {
+    //             throw new Error('No block data found');
+    //         }
+    //     } catch (error) {
+    //         throw new Error(error.message);
     //     }
-    // } catch (error) {
-    //     throw new Error(error.message);
     // }
-// }
+
+    // async updateLastCheckedBlock(blockNumber: number) {
+    //    await this.prismaService.blockchainScan.update({
+    //         where: {
+    //             id: 1,
+    //         },
+    //         data: {
+    //             lastCheckedBlock: blockNumber,
+    //         },
+    //     });
+    // }
 
 }
 
